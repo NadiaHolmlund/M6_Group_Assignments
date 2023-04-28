dab5d9aa511486002e0b03521fcc8f96500e543f
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-04-28 15:36:59 +0200

    test

--- Group_Assignment_4/app.py
+++ Group_Assignment_4/app.py
@@ -6,11 +6,5 @@ st.set_page_config(
     page_icon="⚾",
     layout="wide")
 
-# Setting up the page
-weight = st.number_input('Weight')
-height = st.number_input('Height')
-G = st.number_input('G')
-AB = st.number_input('AB')
-
 # make a nice button that triggers creation of a new data-line in the format that the model expects and prediction
 st.button('Predict! 🚀'):
