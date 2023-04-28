d36f08d21342b88ef779d69de839c7c40d4d8fd3
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-04-28 15:43:24 +0200

    test

--- Group_Assignment_4/app.py
+++ Group_Assignment_4/app.py
@@ -1,4 +1,3 @@
 import streamlit as st
 
-# make a nice button that triggers creation of a new data-line in the format that the model expects and prediction
-st.button('Predict! 🚀'):
+st.head('Hello World')
