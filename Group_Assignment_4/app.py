bb24516be6ccaf74c55203dae78964e2c926352d
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-04-28 13:31:00 +0200

    Update app.py

--- Group_Assignment_4/app.py
+++ Group_Assignment_4/app.py
@@ -1,89 +1,38 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "provenance": [],
-      "authorship_tag": "ABX9TyMvKvnPpou/S+oS8JVzWUc+",
-      "include_colab_link": true
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/NadiaHolmlund/M6_Group_Assignments/blob/main/Group_Assignment_4/app.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
-    {
-      "cell_type": "code",
-      "source": [
-        "!pip install streamlit"
-      ],
-      "metadata": {
-        "id": "9vuNJW1mn5Na"
-      },
-      "execution_count": null,
-      "outputs": []
-    },
-    {
-      "cell_type": "code",
-      "execution_count": null,
-      "metadata": {
-        "id": "nR9yB6FAn4jS"
-      },
-      "outputs": [],
-      "source": [
-        "import streamlit as st\n",
-        "import pickle\n",
-        "\n",
-        "# Setting up page configurations\n",
-        "st.set_page_config(\n",
-        "    page_title=\"HR Prediction\",\n",
-        "    page_icon=\"⚾\",\n",
-        "    layout=\"wide\")\n",
-        "\n",
-        "# Loading the model\n",
-        "@st.experimental_singleton\n",
-        "def read_objects():\n",
-        "    model = pickle.load(open('HR_model/model.pkl','rb'))\n",
-        "\n",
-        "    return model\n",
-        "\n",
-        "model = read_objects()\n",
-        "\n",
-        "def predict():\n",
-        "    data = [[weight, height, G, AB]]\n",
-        "    prediction = model.predict(data)[0]\n",
-        "\n",
-        "# Setting up the page\n",
-        "weight = st.number_input('Weight')\n",
-        "height = st.number_input('Height')\n",
-        "G = st.number_input('G')\n",
-        "ABCMeta = st.number_input('AB')\n",
-        "\n",
-        "# make a nice button that triggers creation of a new data-line in the format that the model expects and prediction\n",
-        "if st.button('Predict! 🚀'):\n",
-        "    # make a DF for categories and transform with one-hot-encoder\n",
-        "    user_input = pd.DataFrame({'weight':weight,'height':height, 'G':G, 'AB':AB}, index=[0])\n",
-        "\n",
-        "    #run prediction for 1 new observation\n",
-        "    predicted_value = model.predict(user_input)[0]\n",
-        "\n",
-        "    #print out result to user\n",
-        "    st.metric(label=\"Predicted HR\", value=f'{round(predicted_value)} kr')"
-      ]
-    }
-  ]
-}
+import streamlit as st
+import pickle
+
+# Setting up page configurations
+st.set_page_config(
+    page_title="HR Prediction",
+    page_icon="⚾",
+    layout="wide")
+
+# Loading the model
+@st.experimental_singleton
+def read_objects():
+    model = pickle.load(open('HR_model/model.pkl','rb'))
+
+    return model
+
+model = read_objects()
+
+def predict():
+    data = [[weight, height, G, AB]]
+    prediction = model.predict(data)[0]
+
+# Setting up the page
+weight = st.number_input('Weight')
+height = st.number_input('Height')
+G = st.number_input('G')
+ABCMeta = st.number_input('AB')
+
+# make a nice button that triggers creation of a new data-line in the format that the model expects and prediction
+if st.button('Predict! 🚀'):
+    # make a DF for categories and transform with one-hot-encoder
+    user_input = pd.DataFrame({'weight':weight,'height':height, 'G':G, 'AB':AB}, index=[0])
+
+    #run prediction for 1 new observation
+    predicted_value = model.predict(user_input)[0]
+
+    #print out result to user
+    st.metric(label="Predicted HR", value=f'{round(predicted_value)} kr')
