c8172c5b280be55302e68a591dc5859b58e52bf0
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-04-28 15:33:18 +0200

    test

--- Group_Assignment_4/app.py
+++ Group_Assignment_4/app.py
@@ -7,32 +7,11 @@ st.set_page_config(
     page_icon="⚾",
     layout="wide")
 
-# Loading the model
-@st.experimental_singleton
-def read_objects():
-    model = pickle.load(open('HR_model/model.pkl','rb'))
-
-    return model
-
-model = read_objects()
-
-def predict():
-    data = [[weight, height, G, AB]]
-    prediction = model.predict(data)[0]
-
 # Setting up the page
 weight = st.number_input('Weight')
 height = st.number_input('Height')
 G = st.number_input('G')
-ABCMeta = st.number_input('AB')
+AB = st.number_input('AB')
 
 # make a nice button that triggers creation of a new data-line in the format that the model expects and prediction
-if st.button('Predict! 🚀'):
-    # make a DF for categories and transform with one-hot-encoder
-    user_input = pd.DataFrame({'weight':weight,'height':height, 'G':G, 'AB':AB}, index=[0])
-
-    #run prediction for 1 new observation
-    predicted_value = model.predict(user_input)[0]
-
-    #print out result to user
-    st.metric(label="Predicted HR", value=f'{round(predicted_value)} kr')
+st.button('Predict! 🚀'):
