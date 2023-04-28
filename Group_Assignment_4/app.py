bf2f23c5d2d01b99fb8fad5237a32dcc0b8cb3ee
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2023-04-28 15:39:27 +0200

    test

--- Group_Assignment_4/app.py
+++ Group_Assignment_4/app.py
@@ -1,10 +1,4 @@
 import streamlit as st
 
-# Setting up page configurations
-st.set_page_config(
-    page_title="HR Prediction",
-    page_icon="⚾",
-    layout="wide")
-
 # make a nice button that triggers creation of a new data-line in the format that the model expects and prediction
 st.button('Predict! 🚀'):
